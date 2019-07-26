c4b2b6b25f5ed5eaf8dcb4b49ecad95d951e3f69
Josh <Joshua.horgen@gmail.com>
2019-07-26 15:25:05 -0700

    solve issues

--- app.rb
+++ app.rb
@@ -10,10 +10,29 @@ get ('/') do
 end
 
 get ('/words') do
-  @boards = Board.all
+  @words = Word.all
   erb(:words)
   end
 
-  post '/definitions' do
-    "Hello World"
-  end
+post ('/words') do
+    word = params[:word]
+    word = Word.new(word)
+    word.save()
+    redirect to('/words')
+end
+
+
+    get ('/words/:id/definitions') do
+      @word_id = params[:id].to_i
+      erb(:definitions)
+    end
+    #
+    # post ('/boards/:id/messages') do
+    #   user = params[:user]
+    #   @board = Board.find(params[:id].to_i)
+    #   message_text = params[:message]
+    #   board_id = params[:id].to_i
+    #   new_message = Definition.new(message_text, word_id)
+    #   new_message.save()
+    #   redirect to("/words/#{word_id}")
+    # end
